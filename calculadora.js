ff2d0ca8a0c0ab4ddc53e7265afcd68a4dc5ec3f
Bruno Tito <brunosantostito2@hotmail.com>
2022-02-01 21:12:18 -0300

    projeto alterado

--- calculadora.js
+++ calculadora.js
@@ -1,29 +1,27 @@
+function calcular(event) {
+    event.preventDefault();
 
-function calcular() {
+    let n1 = document.getElementById('number_one').value
+    let n2 = document.getElementById('number_two').value
 
-    let n1 = document.getElementById('n1').value
-    let n2 = document.getElementById('n2').value
-
-    let operacao = document.getElementById('operacao').value
-
+    let operacao = document.getElementById('operations').value
 
     switch (operacao) {
         case "+":
-            document.getElementById("result").innerHTML = parseFloat(n1) + parseFloat(n2);
+            document.getElementById("result").innerHTML = "O resultado é: " + (parseFloat(n1) + parseFloat(n2));
             break;
 
         case "-":
-            document.getElementById("result").innerHTML = parseFloat(n1) - parseFloat(n2);
+            document.getElementById("result").innerHTML = "O resultado é: " + (parseFloat(n1) - parseFloat(n2));
             break;
 
         case "*":
-            document.getElementById("result").innerHTML = parseFloat(n1) * parseFloat(n2);
+            document.getElementById("result").innerHTML = "O resultado é: " + (parseFloat(n1) * parseFloat(n2));
             break;
 
         case "/":
-            document.getElementById("result").innerHTML = parseFloat(n1) / parseFloat(n2);
+            document.getElementById("result").innerHTML = "O resultado é: " + (parseFloat(n1) / parseFloat(n2));
             break;
     }
 
 };
-
